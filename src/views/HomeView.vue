06d38200c299643ecff3664e3a433320e3129ed1
Franck COLOMBO <franck.colombo@cnav.fr>
2022-10-14 17:37:40 +0200

    suite page actualité

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,8 +20,8 @@ import CardShadow from "@/components/CardShadow.vue";
       </article>
       <!--      A NE PAS LOUPER             -->
       <article class="grid grid-cols-12 gap-5 pr-44 pl-44 pt-32">
-        <h2 class="col-span-full">À ne pas louper</h2>
-        <!-- <div class="col-span-full grid grid-cols-12 gap-5 h-[406px]"> -->
+        <h2 class="col-span-full border-l-bv-green border-l-8  pl-2">À ne pas louper</h2>
+        
           <div class="col-span-7 flex">
             <img
               src="../assets/images/drink-water.png"
@@ -74,17 +74,28 @@ import CardShadow from "@/components/CardShadow.vue";
                   >
             </CardShadow>
           </div>
-          <div class="col-span-4">
-            <img class="rounded-t-md w-full" src="@/assets/images/woman-horses.png" alt="">
-            <CardShadow class="mt-3">
-                  <template v-slot:badge>Ma vie sociale</template>
-                  <template v-slot:title
-                    >Vie associative ou citoyenne : et si je m'engageais ? </template
-                  >
-            </CardShadow>
+          <div class="relative flex flex-col gap-3 col-span-4 bg-slate-200 p-5 pt-[109px] mt-12 rounded-md">
+            <img class="absolute -top-4 left-1/4" src="@/assets/images/computer.svg" alt="">
+            <div class="flex w-fit p-2  pl-3 pr-3 bg-bv-purple rounded-[40px] text-white ">
+              Quizz
+            </div>
+            <h3>Je teste mes connaissances en matière d’alimentation</h3>
+            <div class="flex gap-2 align-middle p-5 pt-3 pb-3 rounded-md bg-white w-max"><img class="w-[13px] -rotate-90" src="@/assets/images/icones/triangle.svg" alt=""> Relever le défi</div>
           </div>
-             
-        <!-- </div> -->
+        <!-- ===== Bien viellir a ma manière ==== -->
+         <div class="col-span-full flex flex-grow p-44 pt-16 pb-20  gap-14 h-[355px] bg-bv-blue-bg">
+            <div class="relative h-full w-[153px]}">
+              <img class=" absolute top-0" src="@/assets/images/ppl-computing.png" alt="">
+              <img class="absolute -top-8 left-40" src="@/assets/images/sun.png" alt="">
+            </div>
+            <div >
+              <h2 class="text-bv-purple">Bien vieillir à ma manière !</h2>
+              <p class="text-bv-grey-2 mt-5  ">Je réponds à un court questionnaire <br/>  et je découvre les articles, conseils et activités qui me correspondent !
+</p>
+<button class="bg-bv-purple text-white rounded-xl p-8 pt-4 pb-4 mt-8">J'y vais !</button>
+            </div>
+         </div>
+    
       </article>
     </section>
   </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       colors:{
         'bv-green': 'rgb(var(--color-green))',
         'bv-grey-2': 'rgb(var(--color-grey-2))',
+        'bv-blue-bg': 'rgb(var(--color-blue-bg))',
         'bv-beige': 'rgb(var(--color-beige))',
         'bv-purple': 'rgb(var(--color-purple))',
       }
