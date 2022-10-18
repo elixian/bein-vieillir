ab48ebd092b97e30a9bf1f986fe4b9b5d0ae8e3d
Franck COLOMBO <franck.colombo@cnav.fr>
2022-10-18 11:07:44 +0200

    add alias images

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@images':fileURLToPath(new URL('./src/assets/images/', import.meta.url))
     }
   }
 })
