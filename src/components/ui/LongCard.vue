905a11e6b2772e2e89331392c045ba6aa302426f
Franck COLOMBO <franck.colombo@cnav.fr>
2022-10-20 14:33:14 +0200

    minor change

--- src/components/ui/LongCard.vue
+++ src/components/ui/LongCard.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="longcard flex rounded-lg shadow-lg h-52  overflow-hidden cursor-pointer">
         <div class="w-[255px] flex-shrink-0 overflow-hidden bg-red-300 ">
-            <img class=" w-[255px] h-full" :src="getImageUrl(url)" alt="">
+            <img class=" w-[255px] h-full" :src="getImageUrl(props.url)" alt="">
         </div>
        
         <div class="p-5 flex flex-col gap-5 bg-white">
-            <h3>{{title}}</h3>
-            <p class="text-bv-grey-2 ">{{content}}</p>
+            <h3>{{props.title}}</h3>
+            <p class="text-bv-grey-2 ">{{props.content}}</p>
         </div>
     </div>
 </template>
