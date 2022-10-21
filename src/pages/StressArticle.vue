849aedae9cc15c0d197aba25534978a94cc577d0
Franck COLOMBO <franck.colombo@cnav.fr>
2022-10-21 11:23:07 +0200

    end StressArticle

--- src/pages/StressArticle.vue
+++ src/pages/StressArticle.vue
@@ -72,9 +72,9 @@ import CardShadow from "@/components/CardShadow.vue";
             </p>
           </article>
           <article class="mt-8">
-            <h3 class="mb-8">La meilleure façon de rester en forme</h3>
+            <h3 >La meilleure façon de rester en forme</h3>
 
-            <p class="mb-6">
+            <p class="mt-5">
               Au moins trente minutes d’exercice par jour, associées à une
               alimentation équilibrée, sont tout indiquées pour améliorer sa
               condition physique, conserver une bonne tonicité musculaire, de la
@@ -85,7 +85,7 @@ import CardShadow from "@/components/CardShadow.vue";
               osseux.
             </p>
             <iframe
-              class="m-auto"
+              class="m-auto mt-5"
               width="560"
               height="315"
               src="https://www.youtube.com/embed/MIDGJVJg8BA"
@@ -96,8 +96,8 @@ import CardShadow from "@/components/CardShadow.vue";
             ></iframe>
           </article>
           <article class="mt-8">
-            <h4 class="mb-8">Un titre dans l'ombre Uxienne</h4>
-            <p class="mb-6">
+            <h4 >Un titre dans l'ombre Uxienne</h4>
+            <p class="mt-5">
               Au moins trente minutes d’exercice par jour, associées à une
               alimentation équilibrée, sont tout indiquées pour améliorer sa
               condition physique, conserver une bonne tonicité musculaire, de la
@@ -141,10 +141,10 @@ import CardShadow from "@/components/CardShadow.vue";
           </a>
 
           <article class="mt-8">
-            <h4 class="mb-8">
+            <h4 >
               Comment maintenir une activité physique régulière ?
             </h4>
-            <p class="mb-6">
+            <p class="mt-5 ">
               La vie quotidienne nous offre bien des occasions de faire de
               l’exercice, autant les saisir ! Vous pouvez ainsi vous fixer des
               objectifs, que vous jugez réalisables, et les tenir à votre propre
@@ -160,7 +160,7 @@ import CardShadow from "@/components/CardShadow.vue";
               qui se distingue de l’activité physique par un effort plus
               soutenu.
             </p>
-            <p class="mb-6">
+            <p class="mt-5">
               Pour une activité régulière, n’hésitez pas à pratiquer en groupe.
               Vous pouvez déjà en parler autour de vous, convaincre votre
               entourage et créer un groupe. Vous pouvez aussi partager votre
@@ -171,103 +171,192 @@ import CardShadow from "@/components/CardShadow.vue";
               davantage et… ensemble ! Pour en savoir plus, découvrez le
               programme D-marche.
             </p>
-            <p class="mb-6">
+            <p class="mt-5">
               Quelques précisions importantes sur le programme :
             </p>
 
             <table>
-                <tr>
-                    <th></th>
-                    <th>Type de la séance</th>
-                    <th>Tarification</th>
-                    <th>Localisation</th>
-                    <th>Prestataire</th>
-                </tr>
-                <tr>
-                    <th>
-                        Lundi
-                    </th>
-                    <td>Technique de mémorisation</td>
-                    <td>Gratuit sur inscription</td>
-                    <td>Paris</td>
-                    <td>Mr GUILLAUME</td>
-                </tr>
-                <tr>
-                    <th>
-                        Mardi
-                    </th>
-                    <td>Penser à son bien-être</td>
-                    <td>Gratuit sur inscription</td>
-                    <td>Lyon</td>
-                    <td>Mr GUILLAUME</td>
-                </tr>
-                <tr>
-                    <th>
-                        Mercredi
-                    </th>
-                    <td>Conférence sur l’alimentation</td>
-                    <td>Gratuit sur inscription</td>
-                    <td>Paris</td>
-                    <td>Mr GUILLAUME</td>
-                </tr>
-                <tr>
-                    <th>
-                        Jeudi
-                    </th>
-                    <td>Technique de mémorisation</td>
-                    <td>Gratuit sur inscription</td>
-                    <td>Paris</td>
-                    <td>Mr GUILLAUME</td>
-                </tr>
+              <tr>
+                <th></th>
+                <th>Type de la séance</th>
+                <th>Tarification</th>
+                <th>Localisation</th>
+                <th>Prestataire</th>
+              </tr>
+              <tr>
+                <th>Lundi</th>
+                <td>Technique de mémorisation</td>
+                <td>Gratuit sur inscription</td>
+                <td>Paris</td>
+                <td>Mr GUILLAUME</td>
+              </tr>
+              <tr>
+                <th>Mardi</th>
+                <td>Penser à son bien-être</td>
+                <td>Gratuit sur inscription</td>
+                <td>Lyon</td>
+                <td>Mr GUILLAUME</td>
+              </tr>
+              <tr>
+                <th>Mercredi</th>
+                <td>Conférence sur l’alimentation</td>
+                <td>Gratuit sur inscription</td>
+                <td>Paris</td>
+                <td>Mr GUILLAUME</td>
+              </tr>
+              <tr>
+                <th>Jeudi</th>
+                <td>Technique de mémorisation</td>
+                <td>Gratuit sur inscription</td>
+                <td>Paris</td>
+                <td>Mr GUILLAUME</td>
+              </tr>
             </table>
           </article>
           <article class="mt-8">
-            <h4 class="mb-8">
-              La bonne idée
-            </h4>
-            <p>Intégrer un club permet de profiter de conseils personnalisés d’animateurs formés au sport senior. Les choix sont nombreux : allant de séances multisports à des activités comme le fit’ball, le cardio training, le step, la danse country, le tai chi, sans oublier les cours thématiques comme Gymmémoire, l’Equilibre où en êtes-vous ? Sur le site de la FFEPGV par exemple vous trouvez tous les détails ainsi qu’une carte de France pour identifier les clubs dans votre région. </p>
+            <h4 >La bonne idée</h4>
+            <p class="mt-5">
+              Intégrer un club permet de profiter de conseils personnalisés
+              d’animateurs formés au sport senior. Les choix sont nombreux :
+              allant de séances multisports à des activités comme le fit’ball,
+              le cardio training, le step, la danse country, le tai chi, sans
+              oublier les cours thématiques comme Gymmémoire, l’Equilibre où en
+              êtes-vous ? Sur le site de la FFEPGV par exemple vous trouvez tous
+              les détails ainsi qu’une carte de France pour identifier les clubs
+              dans votre région.
+            </p>
           </article>
 
           <article class="mt-8">
             <div class="bg-bv-beige p-6 border border-bv-green rounded-lg">
-                <h4>Le saviez vous ?</h4>
-                <p class="mt-8">Les bénéfices de l’activité physique sont de 4 sortes : l’endurance, le renforcement musculaire, la souplesse et l’équilibre. Pour découvrir les différentes activités et la fréquence à laquelle les pratiquer, suivez ce lien. Et vous apprendrez que l’activité physique est bonne pour la tête également ! </p>
+              <h4>Le saviez vous ?</h4>
+              <p class="mt-5">
+                Les bénéfices de l’activité physique sont de 4 sortes :
+                l’endurance, le renforcement musculaire, la souplesse et
+                l’équilibre. Pour découvrir les différentes activités et la
+                fréquence à laquelle les pratiquer, suivez ce lien. Et vous
+                apprendrez que l’activité physique est bonne pour la tête
+                également !
+              </p>
             </div>
           </article>
 
           <article class="mt-8">
-            <h4> Pour aller plus loin</h4>
-            <p class="mt-5">Si vous désirez creuser ce sujet nous vous proposons ce support en format pdf téléchargeable via le lien ci-dessous :</p>
-            <a class="flex  gap-3 p-2 pl-3 pr-3 mt-4 border border-bv-purple rounded-lg w-max" href="/static/Programme-sport.pdf" download >
-               <img src="@images/icones/download.svg" alt="telechargement icone"> Programme de sport - Bien vieillir [PDF - 2 Mo] 
+            <h4>Pour aller plus loin</h4>
+            <p class="mt-5">
+              Si vous désirez creuser ce sujet nous vous proposons ce support en
+              format pdf téléchargeable via le lien ci-dessous :
+            </p>
+            <a
+              class="flex gap-3 p-2 pl-3 pr-3 mt-4 border border-bv-purple rounded-lg w-max"
+              href="/static/Programme-sport.pdf"
+              download
+            >
+              <img
+                src="@images/icones/download.svg"
+                alt="telechargement icone"
+              />
+              Programme de sport - Bien vieillir [PDF - 2 Mo]
             </a>
-        </article >
-        <article class="mt-8">
-            <div class="bg-bv-blue-bg rounded-lg p-4">
-                <h3 class="text-bv-green">Lien utiles</h3>
-                <div class="flex p-4 pt-5 pb-5 gap-5 bg-white rounded-lg  ">
-                    <img src="@images/logo-federation-natio-sport.png" alt="">
-                    <div>
-                        <h5 class="underline mb-2 ">Sport en milieu rural</h5>
-                        <p>La FNSMR a pour but de favoriser la pratique d’activités sportives adaptées aux spécificités de milieu rural.</p>
-                        
-                    </div>
-                    <img class="w-4" src="@images/icones/chevron-right.svg" alt="">
+          </article>
+          <article class="mt-8">
+            <div
+              class="bg-bv-blue-bg rounded-lg p-4 pt-6 pb-6 flex flex-col gap-5"
+            >
+              <h3 class="text-bv-green">Lien utiles</h3>
+              <div class="flex p-4 pt-5 pb-5 gap-5 bg-white rounded-lg">
+                <img src="@images/logo-federation-natio-sport.png" alt="" />
+                <div>
+                  <h5 class="underline mb-2">Sport en milieu rural</h5>
+                  <p>
+                    La FNSMR a pour but de favoriser la pratique d’activités
+                    sportives adaptées aux spécificités de milieu rural.
+                  </p>
                 </div>
+                <img
+                  class="w-4"
+                  src="@images/icones/chevron-right.svg"
+                  alt=""
+                />
+              </div>
+              <div class="flex p-4 pt-5 pb-5 gap-5 bg-white rounded-lg">
+                <img src="@images/logo-federation-natio-sport.png" alt="" />
+                <div>
+                  <h5 class="underline mb-2">Sport en milieu rural</h5>
+                  <p>
+                    La FNSMR a pour but de favoriser la pratique d’activités
+                    sportives adaptées aux spécificités de milieu rural.
+                  </p>
+                </div>
+                <img
+                  class="w-4"
+                  src="@images/icones/chevron-right.svg"
+                  alt=""
+                />
+              </div>
+              <div class="flex p-4 pt-5 pb-5 gap-5 bg-white rounded-lg">
+                <img src="@images/logo-federation-natio-sport.png" alt="" />
+                <div>
+                  <h5 class="underline mb-2">Sport en milieu rural</h5>
+                  <p>
+                    La FNSMR a pour but de favoriser la pratique d’activités
+                    sportives adaptées aux spécificités de milieu rural.
+                  </p>
+                </div>
+                <img
+                  class="w-4"
+                  src="@images/icones/chevron-right.svg"
+                  alt=""
+                />
+              </div>
             </div>
-        </article>
-          
+          </article>
+          <article class="mt-8">
+            <h3>C’est le moment de tester vos connaissances !</h3>
+            <div
+              class="flex mt-6 rounded-lg shadow-lg h-[228px] bg-blue-200 p-8 overflow-hidden cursor-pointer"
+            >
+              <div class="flex-shrink-0">
+                <img class="h-full" src="@images/computer.svg" alt="" />
+              </div>
+
+              <div class="pl-5 flex gap-4 flex-col">
+                <div
+                  class="flex w-fit p-2 pl-3 pr-3 bg-blue-600 rounded-[40px] text-white"
+                >
+                  Quizz
+                </div>
+                <h4>Testez vos conaissances en matière de gestion du stress</h4>
+                <div
+                  class="flex gap-2 align-middle p-5 pt-3 pb-3 rounded-md bg-white w-max"
+                >
+                  <img
+                    class="w-[13px] -rotate-90"
+                    src="@/assets/images/icones/triangle.svg"
+                    alt=""
+                  />
+                  Relever le défi
+                </div>
+              </div>
+            </div>
+          </article>
         </section>
+        <div class="flex gap-5 mt-8">
+          
+          <a class="flex basis-52 w-min ml-auto gap-5 p-2 text-bv-purple rounded-lg border border-bv-purple  " href="#"><img src="@images/icones/share-link.svg" alt=""><span class="flex-shrink" >Partager cet article</span></a>
+          <a class="flex basis-52 gap-5 p-2 text-bv-purple rounded-lg border border-bv-purple w-max " href="#"><img src="@images/icones/printer.svg" alt=""><span >Imprimer cet article</span></a>
+
+        </div>
       </main>
     </div>
-    <article class="grid grid-cols-12 gap-5 pt-24">
-      <h2 class="col-span-full">Les dernières actualités sur le sujet</h2>
+    <article class="grid grid-cols-12 gap-5 pt-24 pb-20">
+      <h2 class="col-span-full border-l-8 pl-2 border-bv-green">Les dernières actualités sur le sujet</h2>
 
       <CardShadow class="col-span-3">
         <template v-slot:image>
           <img
             class="rounded-t-md w-full"
-            src="@/assets/images/myhead/actu-1.png"
+            src="@/assets/images/stress/connex-1.webp"
             alt=""
           />
         </template>
@@ -279,7 +368,7 @@ import CardShadow from "@/components/CardShadow.vue";
         <template v-slot:image>
           <img
             class="rounded-t-md w-full"
-            src="@/assets/images/myhead/actu-2.png"
+            src="@/assets/images/stress/connex-2.webp"
             alt=""
           />
         </template>
@@ -291,7 +380,7 @@ import CardShadow from "@/components/CardShadow.vue";
         <template v-slot:image>
           <img
             class="rounded-t-md w-full"
-            src="@/assets/images/myhead/actu-3.png"
+            src="@/assets/images/stress/connex-3.webp"
             alt=""
           />
         </template>
@@ -303,7 +392,7 @@ import CardShadow from "@/components/CardShadow.vue";
         <template v-slot:image>
           <img
             class="rounded-t-md w-full"
-            src="@/assets/images/myhead/actu-4.png"
+            src="@/assets/images/stress/connex-4.webp"
             alt=""
           />
         </template>
@@ -313,21 +402,45 @@ import CardShadow from "@/components/CardShadow.vue";
       </CardShadow>
     </article>
   </LayoutFull>
+  <div class=" bg-bv-blue-bg pt-10 pb-10 ">
+    <LayoutFull >
+      <h2 class="border-l-8 pl-2 border-bv-green">Suggestions d’autres contenus</h2>
+      <div class="grid grid-cols-12 gap-5 mt-8 ">
+        <CardShadow class="col-span-3 rounded-lg shadow-lg">
+         
+          <template v-slot:badge>Bien-être</template>
+          <template v-slot:title>Sport en milieu rural</template>
+        </CardShadow>
+        <CardShadow class="col-span-3 rounded-lg shadow-lg">
+         
+          <template v-slot:badge>Actualité</template>
+          <template v-slot:title>Se consacrer à une association</template>
+        </CardShadow>
+        <CardShadow class="col-span-3 rounded-lg shadow-lg">
+         
+          <template v-slot:badge>Actualité</template>
+          <template v-slot:title>Témoignage de Xavier</template>
+        </CardShadow>
+        <CardShadow class="col-span-3 rounded-lg shadow-lg">
+         
+          <template v-slot:badge>Actualité</template>
+          <template v-slot:title>Covid-19 </template>
+        </CardShadow>
+        
+      </div>
+    </LayoutFull>
+  </div>
 </template>
 
-
 <style lang="scss" scoped>
-table  {
-    
-    tr td ,th{
-
-        border: 1px solid black;
-        padding:10px
-    }
-    :first-child  th:first-of-type{
-        border:none;
-    }
-   
-
+table {
+  tr td,
+  th {
+    border: 1px solid black;
+    padding: 10px;
+  }
+  :first-child th:first-of-type {
+    border: none;
+  }
 }
 </style>
