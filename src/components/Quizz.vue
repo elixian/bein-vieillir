c0d4d7464f914c6ebcf6021a3fed725ec538c235
Franck COLOMBO <franck.colombo@cnav.fr>
2022-10-21 17:38:32 +0200

    gestion des états

--- src/components/Quizz.vue
+++ src/components/Quizz.vue
@@ -1,45 +1,82 @@
 <script setup>
-import { ref,unref } from "vue";
+import { ref, unref } from "vue";
 
 const props = defineProps({ q_data: Object });
 console.log("🚀 ~ file: Quizz.vue ~ line 3 ~ props", props);
 const index = ref(0);
 
-const selected = ref("")
+const selected = ref(null);
 
-
+const nextButton = ref(0);
 
 // Enregistrer le resultat a la validation
-const goodanswers = ref([])
+const goodanswers = ref([]);
 
 // function checkResponse
-function checkResponse(){
-   
-    console.log(props.q_data[index.value].response === +selected.value)
-    goodanswers.value.push(props.q_data[index.value].response === +selected.value)
+function checkResponse() {
+  goodanswers.value.push(
+    props.q_data[index.value].response === +selected.value
+  );
+  nextButton.value = 1;
 }
 
+function nextQuestion() {
+  index.value = index.value + 1;
+  nextButton.value = !nextButton.value;
+  selected.value = null;
+}
 </script>
 
 <template>
-  <div>
-    <h1>QUIZZ</h1>
-
+  <div class="shadow-lg p-5 bg-white rounded-lg">
     <div>
+
       <transition name="slide-fade" mode="out-in">
+        
         <div :key="q_data[index].id">
-            <h2>{{ q_data[index].title }}</h2>
-            <!-- <div v-for="n in q_data.choix" :key="n.id">{{n.choice}}</div> -->
+            <p>Question {{ index +1 }} sur {{q_data.length}}</p>
+          <h3>{{ q_data[index].title }}</h3>
+        </div>
+    </transition>
+    <div class="border border-spacing-3 border-bv-green mb-4"></div>
+    <transition name="slide-fade" mode="out-in">
+        <div :key="q_data[index].id">
+          <!-- <div v-for="n in q_data.choix" :key="n.id">{{n.choice}}</div> -->
+          <div class="flex flex-col gap-4">
             <template v-for="n in q_data[index].choix" :key="n.id">
-              <input type="radio" :value="n.id" :name="q_data.id" :id="n.id" v-model="selected"/>
-              <label :for="n.id">{{ n.choice }}</label>
+              <label class="border p-3 rounded-lg hover:bg-bv-blue-bg cursor-pointer" :for="n.id">
+                <input
+                  type="radio"
+                  :value="n.id"
+                  :name="q_data.id"
+                  :id="n.id"
+                  v-model="selected"
+                />
+                {{ n.choice }}
+              </label>
             </template>
+          </div>
         </div>
       </transition>
-      <p>v-model : {{selected}}</p>
+      <p>v-model : {{ selected }}</p>
     </div>
-    <button @click.prevent="index--">Previous</button>
-    <button @click.prevent="checkResponse">Valider ma réponse</button>
+    <a class="underline p-3 mr-3" href="#" @click.prevent="index--">
+        Retour à la question précédente
+    </a>
+    <button
+      v-if="!nextButton"
+      class="border rounded-r-lg p-3 mr-3"
+      @click.prevent="checkResponse"
+    >
+      Valider ma réponse
+    </button>
+    <button
+      v-else
+      class="border rounded-r-lg p-3 mr-3"
+      @click.prevent="nextQuestion()"
+    >
+      Question suivante
+    </button>
   </div>
 </template>
 
--- src/pages/QuizzPage.vue
+++ src/pages/QuizzPage.vue
@@ -1,6 +1,7 @@
 <script setup>
  import data_quizz from "@/data/quizz.json"
  import Quizz from "@/components/Quizz.vue"
+ import LayoutFull from "@/layouts/LayoutFull.vue"
 const getAlimentation = ()=>{
     const {alimentation} = data_quizz;
     console.log("🚀 ~ file: QuizzPage.vue ~ line 5 ~ getAlimentation ~ alimenation", alimentation)
@@ -11,7 +12,21 @@ console.log("🚀 ~ file: QuizzPage.vue ~ line 9 ~ questions", questions)
 </script>
 
 <template>
-    <div>
-        <Quizz :q_data="questions" />
-    </div>
+        <div class="absolute bg-bv-blue-bg h-80 w-full"></div>
+        <LayoutFull class="pt-5 pb-52">
+            Accueil > <span>Bien dans mon corps</span>
+            
+
+            <div class="grid grid-cols-12 mt-5">
+                <div class="col-start-3 col-end-11">
+                    <h2>Qwizz - Alimentation</h2>
+                    
+                    <p class="text-bv-grey-2 mt-5 ">Savez-vous vraiment tout ce qu’il faut savoir pour avoir une alimentation saine ? 
+                    On vous met au défi !</p>
+                    <Quizz class=" mt-5" :q_data="questions" />
+                </div>
+                
+            </div>
+        </LayoutFull>
+    
 </template>
