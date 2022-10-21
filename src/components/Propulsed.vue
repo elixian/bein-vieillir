026959ab321c30da78d5c36da174f505e339064e
Franck COLOMBO <franck.colombo@cnav.fr>
2022-10-21 11:22:35 +0200

    remove margin

--- src/components/Propulsed.vue
+++ src/components/Propulsed.vue
@@ -1,5 +1,5 @@
 <template>
-      <div class="flex justify-center items-center h-24 gap-10 mt-32 bg-slate-50">
+      <div class="flex justify-center items-center h-24 gap-10  bg-slate-50">
     <p>Proposé par:</p>
     <img src="@images/canv-logo.png" alt="Assurance retraite logo">
     <img src="@images/logo-agirc.png" alt="Agirc logo">
