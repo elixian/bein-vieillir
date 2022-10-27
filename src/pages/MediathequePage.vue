fdfdb7d2728fdeca2f978f7842f83a737950360e
Franck COLOMBO <franck.colombo@cnav.fr>
2022-10-27 12:49:46 +0200

    mediatheque

---
+++ src/pages/MediathequePage.vue
@@ -0,0 +1,30 @@
+<script setup>
+import LayoutFull from "@/layouts/LayoutFull.vue";
+</script>
+
+<template>
+  <div class="bg-bv-blue-bg pt-8 pb-8">
+    <LayoutFull>
+      <div class="breadcrumb col-span-full mb-10">
+        <router-link class="hover:text-bv-green transition" to="/"
+          >Accueil</router-link
+        >
+        >
+        <span class="font-bold">Médiathèque</span>
+      </div>
+      <div class="grid grid-cols-12">
+        <div class="col-start-2 col-end-12">
+          <h1 class="text-3xl text-bv-green mb-5">Médiathèque</h1>
+          <p class="text-lg">
+            Ici sont réunies toutes les brochures grand public Bien vieillir
+            présentes sur le site. Elles s’adressent aux seniors et personnes
+            âgées, mais aussi aux aidants et sont gratuites, disponibles en
+            téléchargement et à la commande. Réalisées avec différents
+            partenaires, elles apportent les clés pour un vieillissement en
+            santé.
+          </p>
+        </div>
+      </div>
+    </LayoutFull>
+  </div>
+</template>
