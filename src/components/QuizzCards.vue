c2b47c220f5a61d465a2e2ebb28ce2d1f3772985
Franck COLOMBO <franck.colombo@cnav.fr>
2022-10-28 15:05:20 +0200

    amélioration gestion des év'`enements

--- src/components/QuizzCards.vue
+++ src/components/QuizzCards.vue
@@ -5,64 +5,56 @@ import StepperUi from "./ui/StepperUi.vue";
 import CheckboxCard from "./ui/CheckboxCard.vue";
 
 const props = defineProps({ q_data: Object });
-
-const selectedCards = ref([])
-
+const selectedCards = ref([]);
 const index = ref(0);
 
-
-   function updateValue(e){
-    console.log(e);
-    selectedCards.value.find(v => v == e)? selectedCards.value = selectedCards.value.filter(n => n != e): selectedCards.value.push(+e)
-
-    
-    }
+function updateValue(e) {
+  selectedCards.value.find((v) => v == e)
+    ? (selectedCards.value = selectedCards.value.filter((n) => n != e))
+    : selectedCards.value.push(+e);
+}
 </script>
 
 <template>
-      <div class="shadow-lg p-5 bg-white rounded-lg">
+  <div class="shadow-lg p-5 bg-white rounded-lg">
     <div>
-        <!-- {{test}} -->
-        <div :key="q_data[index].id ">
-            <p class="text-center text-bv-green font-bold">Question 
-              <transition name="slide-fade" mode="out-in">
-                <span>{{ index +1 }} </span>
-              </transition>
-                sur {{q_data.length}}</p>
-          <h3 class="mt-4">{{ q_data[index].title }}</h3>
-          {{selectedCards}}
-        </div>
-   
-    <stepper-ui :percentStage="percentStage"></stepper-ui>
-    <transition name="slide-fade" mode="out-in">
+      <!-- {{test}} -->
+      <div :key="q_data[index].id">
+        <p class="text-center text-bv-green font-bold">
+          Question
+          <transition name="slide-fade" mode="out-in">
+            <span>{{ index + 1 }} </span>
+          </transition>
+          sur {{ q_data.length }}
+        </p>
+        <h3 class="mt-4">{{ q_data[index].title }}</h3>
+        {{ selectedCards }}
+      </div>
+
+      <stepper-ui :percentStage="percentStage"></stepper-ui>
+      <transition name="slide-fade" mode="out-in">
         <div :key="q_data[index].id">
           <!-- <div v-for="n in q_data.choix" :key="n.id">{{n.choice}}</div> -->
           <div class="grid grid-cols-4 gap-4">
             <template v-for="n in q_data[index].choix" :key="n.id">
-              
-                <checkbox-card :data="n" v-on:modelValue="updateValue($event)">
-                    <template v-slot:image>
-                        <img src="@images/quizzCards/game-society.svg" alt="">
-                    </template>
-                    <template v-slot:label>
-                            {{n.choice}}
-                    </template>
-                    <template v-slot:input>
-                        
-                    </template>
-                </checkbox-card>
-              
+              <checkbox-card :data="n" v-on:modelValue="updateValue($event)">
+                <template v-slot:image>
+                  <img src="@images/quizzCards/game-society.svg" alt="" />
+                </template>
+                <template v-slot:label>
+                  {{ n.choice }}
+                </template>
+                <template v-slot:input> </template>
+              </checkbox-card>
             </template>
           </div>
         </div>
-       
       </transition>
-   
     </div>
 
     <!-- ================ CONTROLS BUTTONS =============== -->
     <div class="flex justify-end gap-5 mt-8">
-        <!-- <a class="underline p-3 mr-3" v-if=" index>0" href="#" @click.prevent="index--">
+      <!-- <a class="underline p-3 mr-3" v-if=" index>0" href="#" @click.prevent="index--">
             Retour à la question précédente
         </a>
         <button
--- src/components/ui/CheckboxCard.vue
+++ src/components/ui/CheckboxCard.vue
@@ -1,11 +1,15 @@
 <template>
-    <label class=" flex flex-col items-center p-4 gap-4 border border-bv-grey-2 rounded-lg min-h-full cursor-pointer" :for="data.id">
-        <div class="h-10 w-10 rounded-full bg-slate-100  "><slot name="image"></slot></div>
-        <p class="flex-grow"><slot name="label"></slot></p>
-        <slot name="input"><input type="checkbox" :value="data.id" :id="data.id" aria-label="test"
+    <div class="relative flex flex-col items-center p-4 gap-4  min-h-full cursor-pointer transition " @click="toggleCLass">
+    <label class="absolute top-0 lef-0 h-full w-full  z-10 border border-bv-grey-2 rounded-lg cursor-pointer" :for="data.id" ></label>
+    <div class="h-10 w-10 rounded-full bg-slate-100  "><slot name="image"></slot></div>
+    <p class="flex-grow">
+        <slot  name="label"></slot>
+    </p>
+    <slot name="input"><input class="flex-grow select-none pointer-events-none" type="checkbox" :value="data.id" :id="data.id" aria-label="test"
 @input="(e)=> $emit('modelValue', e.target.value)"></slot>
-        
-    </label>
+          
+    </div>
+
 </template>
 
 <script setup>
@@ -15,7 +19,23 @@ import { computed } from "vue-demi"
         data:Object
     })
 
-    computed(()=>{
+    function toggleCLass(e){
+        console.log("🚀 ~ file: CheckboxCard.vue ~ line 19 ~ toggleCLass ~ e", e)
+        e.target.classList.toggle('selected')
+    }
 
-    })
+
 </script>
+
+
+<style lang="scss" scoped>
+.selected{
+    transition: all .3s ease-in-out;
+    background: rgba(0, 166, 226, 0.1);
+    border : 3px solid #3455DB;
+    & > div {
+        background: white;
+    }
+}
+
+</style>
